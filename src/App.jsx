6650dd18db086d40003dfb8934e5fc8048584514
Pratikkk <kenipratik9@gmail.com>
2023-11-22 15:13:17 +0530

    added filterning tasks

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,23 @@ import Todo from "./components/Todo";
 import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
 
-function App(props) {
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
 
-  const [tasks, setTasks] = useState(props.tasks)
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
   function addTask(name) {
-    const newTask = { id: "todo-" + Math.random(), name: name, completed: false };
+    const newTask = {
+      id: "todo-" + Math.random(),
+      name: name,
+      completed: false,
+    };
     setTasks([...tasks, newTask]);
   }
   function toggleTaskCompleted(id) {
@@ -35,7 +47,7 @@ function App(props) {
     setTasks(editedTaskList);
   }
 
-  const taskList = tasks.map((task) => (
+  const taskList = tasks.filter(FILTER_MAP[filter]).map((task) => (
     <Todo
       key={task.id}
       id={task.id}
@@ -45,19 +57,24 @@ function App(props) {
       deleteTask={deleteTask}
       editTask={editTask}
     />
-
-  ));
+  ))
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
+      <div className="filters btn-group stack-exception">{filterList}</div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
