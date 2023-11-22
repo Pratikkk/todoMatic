9a73d78e84c37def30bfdaadb4f71461897b4c4b
Pratikkk <kenipratik9@gmail.com>
2023-11-22 12:29:37 +0530

    componentlize app parts

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const DATA = [
+  {
+    name: "Eat",
+    completed: true,
+    id: "todo-0",
+  },
+  {
+    name: "Sleep",
+    completed: false,
+    id: "todo-1",
+  },
+  {
+    name: "Repeat",
+    completed: false,
+    id: "todo-2",
+  },
+  {
+    name: "Work",
+    completed: false,
+    id: "todo-3",
+  },
+];
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App  />
+    <App tasks={DATA} />
   </React.StrictMode>,
-)
+);
